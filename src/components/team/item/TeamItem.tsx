b3475cfa0368e9175c5d4068767f51bdab0e6536
Vinh-Phuc Hoang Truong <truonghoangvinhphuc@gmail.com>
2023-04-20 00:56:17 +0300

    Feat: Add Team components

---
+++ src/components/team/item/TeamItem.tsx
@@ -0,0 +1,7 @@
+import "./TeamItem.scss";
+
+function TeamItem() {
+  return <div>Team Item component</div>;
+}
+
+export default TeamItem;
---
+++ src/components/team/list/TeamList.tsx
@@ -0,0 +1,8 @@
+import TeamItem from "../item/TeamItem";
+import "./TeamList.scss";
+
+function TeamList() {
+  return <TeamItem />;
+}
+
+export default TeamList;
