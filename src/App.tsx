cd68e2c1701cca535363d36aae700b3ce55c551e
Vinh-Phuc Hoang Truong <truonghoangvinhphuc@gmail.com>
2023-04-20 00:55:59 +0300

    Fix: Conflicted ESLint rules & Clear unused stuffs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.scss";
+import TeamList from "components/team/list/TeamList";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div className="m-8">
+      <h1 className="text-8xl">JOIN A TEAM</h1>
+      <p className="text-2xl">Apply for a role that suits you the most and join a team.</p>
+      <TeamList />
     </div>
   );
 }
